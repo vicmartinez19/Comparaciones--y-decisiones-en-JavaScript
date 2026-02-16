22be71be67cc4d33ee20a183b8c9ad7f75ad523c
vicmartinez <vmmartinez.infomec19@gmail.com>
2026-02-15 21:16:08 -0600

    Feat: Se agrega las evaluacipnes de excelente, bien, sufienciente y no aprobado

---
+++ comparaciones.js
@@ -0,0 +1,16 @@
+let nota = 75;
+
+if (nota > 0 && nota <= 100) {
+    console.log("La nota del estudiante es:", nota);
+
+    if (nota >= 90) {
+        console.log("Resultado: Excelente");
+    } else if (nota >= 75 && nota <= 89) {
+        console.log("Resultado: Bien ");
+    } else if (nota >= 60 && nota <= 74) {
+        console.log("Resultado: Suficiente");
+    } else {
+        console.log("Resultado: No aprobado");
+    }
+
+} 
